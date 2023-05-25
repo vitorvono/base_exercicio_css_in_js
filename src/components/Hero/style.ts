4a88f5ff284795c2c140faa3b77e255b8f83511a
Vitor Vono <vitorvono@gmail.com>
2023-05-25 15:23:52 -0300

    Create style.ts

---
+++ src/components/Hero/style.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import { Container } from '../../styles'
+
+export const HeroDisplay = styled.section`
+  height: 360px;
+  width: 100%;
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  ::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${(props) => props.theme.colorPrimary};
+    content: '';
+    opacity: 0.7;
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
+  }
+`
+
+export const HeroContainer = styled(Container)`
+  position: relative;
+  color: ${(props) => props.theme.colorLight};
+`
+
+export const HeroTitle = styled.h2`
+  font-family: Gloock, serif;
+  font-size: 48px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`
