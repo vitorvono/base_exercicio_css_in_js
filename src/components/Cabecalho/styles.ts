854c3e73bec97251b118f58c8cba6ba8e1b90629
Vitor Vono <vitorvono@gmail.com>
2023-05-25 15:19:38 -0300

    Create styles.ts

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  background-color: ${(props) => props.theme.colorSecondary};
+  color: ${(props) => props.theme.colorPrimary};
+  text-align: center;
+  padding: 24px 0;
+`
