a76dd15271667eac9350681e50aa21a807ed85fb
Vitor Vono <vitorvono@gmail.com>
2023-05-25 15:16:07 -0300

    css.module to styled components

---
+++ src/themePrimary.ts
@@ -0,0 +1,12 @@
+const themePrimary = {
+  colorPrimary: '#a7727d',
+  colorSecondary: '#f9f5e7',
+  colorLight: '#eee'
+}
+
+export default themePrimary
+
+export type Theme = {
+  colorPrimary: string
+  colorSecondary: string
+}
