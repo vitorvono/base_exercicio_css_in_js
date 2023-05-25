79c01dea1c450982f21696b284b05d4fadd3267d
Vitor Vono <vitorvono@gmail.com>
2023-05-25 15:20:50 -0300

    Update index.tsx

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { SearchBtn, SearchForm, SearchInputField } from './styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +14,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <SearchForm onSubmit={aoEnviarForm}>
+      <SearchInputField
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <SearchBtn type="submit">Pesquisar</SearchBtn>
+    </SearchForm>
   )
 }
 export default FormVagas
